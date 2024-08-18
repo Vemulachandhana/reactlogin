505eb66abc6c77d554f393920dc3ad1c45d894ea
Chandhana <chandhanac73@gmail.com>
2024-08-18 16:14:14 +0530

    react login

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+import Footer from './components/footer'
+import Netflixlogin from './components/Nlogin'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Netflixlogin></Netflixlogin>
+        <Footer></Footer>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
